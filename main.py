2a46765e6f77bb1d7a342a823192401bfa3dbda4
Dhafer <ghaferkhalfaoui9@gmail.com>
2026-02-09 18:20:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 import threading
-import start_sniffing from sniffer
+from sniffer import start_sniffing
+from port_scanner import runner
+import time 
+
+def main() :
+    thread = threading.Thread(target = start_sniffing,daemon=True )
+    thread.start()
+    runner("127.0.0.1", range(1, 1024))
+    while (True):
+        time.sleep(1)
+        
+    
+
+if __name__ == "__main__":
+    main()
