e5f380dc10cfeae0e3b0dfaa35ee0ce3a464e8e6
Dhafer <ghaferkhalfaoui9@gmail.com>
2026-02-09 19:19:27 +0100

    Update port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -1,39 +1,25 @@
 import socket
 import concurrent.futures
-import itertools
-    
-    	
-def scan_port(ip,port):
-	target = ip
-	
-	sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM )
-	sock.settimeout(1)
-	try:
-		result = sock.connect_ex((target,port))
-		if result == 0:
-            		print(f"port is open {port} ip : {target}")
-            		try:
-            			banner = sock.recv(1024)
-            			banner = banner.decode().strip()
-            			print(f"open port : {port} service {banner}")
-            		except:
-            			print(f"open port : {port} service unknown")
-            			
-        
-	except Exception as e:
-		print(f"error scanning port {port}: {e}")
-	sock.close()
-	
-	
-def runner(targets,ports):
-     
+from shared import scanned_ports
 
+
+def scan_port(ip, port):
+    # Register the port as being scanned
+    scanned_ports.add(port)
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(1)
+
+    try:
+        result = sock.connect_ex((ip, port))
+        if result == 0:
+            print(f"[SCANNER] OPEN {ip}:{port}")
+    except:
+        pass
+
+    sock.close()
+
+
+def runner(target, ports):
     with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
-    	executor.map(scan_port, itertools.repeat(targets),ports)
-
-
-
-
-
-
-
+        executor.map(scan_port, [target] * len(ports), ports)
